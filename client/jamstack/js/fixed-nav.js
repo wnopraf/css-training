88fec1c796021d4c3d6bf3289689e15a2d135594
wnpraf <rapribu@gmail.com>
2018-03-31 21:40:36 +0200

    adding sroll nav component

---
+++ client/jamstack/js/fixed-nav.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', (e) => {
+   const nav = document.querySelector('.nav-part')
+   const scroller = e.target
+   if (scroller.scrollTop > 0) {
+       nav.classList.add('on-scroll-nav')
+   } else {
+       nav.classList.remove('on-scroll-nav')
+   }
+})
