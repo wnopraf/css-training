89f1f5694604f9428c3bf014ad11a8f425655f2e
wnpraf <rapribu@gmail.com>
2018-04-01 17:36:39 +0200

    finishing header component

--- client/jamstack/js/fixed-nav.js
+++ client/jamstack/js/fixed-nav.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', (e) => {
    const nav = document.querySelector('.nav-part')
-   const scroller = e.target
+   const scroller = e.target.documentElement
    if (scroller.scrollTop > 0) {
        nav.classList.add('on-scroll-nav')
    } else {
