4aa7895e1034d8580ccf15acad82dcf7ddaec152
wnpraf <rapribu@gmail.com>
2018-04-08 20:27:41 +0200

    calculating nav height programmatically

--- client/jamstack/js/nav-mobile.js
+++ client/jamstack/js/nav-mobile.js
@@ -1,7 +1,14 @@
 const actionMenu = document.querySelector('.mobile-action-menu')
 const mobileMenu = document.querySelector('.mobile-menu')
-
+const responiveMenu = document.querySelector('.responsive-menu')
+mobileMenu.style.height = '0px'
 actionMenu.addEventListener('click',(e) => {
+    let computedHeight = window.getComputedStyle(responiveMenu).height
+    console.log(computedHeight,'com height');
+    if (mobileMenu.style.height === '0px') {
+        mobileMenu.style.height = computedHeight
+    } else {
+        mobileMenu.style.height = '0px'
+    }
     
-    mobileMenu.classList.toggle('mobile-menu-is-visible')
 })
