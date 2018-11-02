fcf311bffddbca1d653a9106d077224694438375
wnpraf <rapribu@gmail.com>
2018-11-02 20:15:08 +0100

    adding standard style to proyect. finalizing slider, responsive slider,explore and contact first mobile sections

--- client/jamstack/js/fixed-nav.js
+++ client/jamstack/js/fixed-nav.js
@@ -1,9 +1,9 @@
-window.addEventListener('scroll', (e) => {
-   const nav = document.querySelector('.nav-part')
-   const scroller = e.target.documentElement
-   if (scroller.scrollTop > 0) {
-       nav.classList.add('on-scroll-nav')
-   } else {
-       nav.classList.remove('on-scroll-nav')
-   }
+window.addEventListener('scroll', e => {
+  const nav = document.querySelector('.nav-part')
+  const scroller = e.target.documentElement
+  if (scroller.scrollTop > 0) {
+    nav.classList.add('on-scroll-nav')
+  } else {
+    nav.classList.remove('on-scroll-nav')
+  }
 })
